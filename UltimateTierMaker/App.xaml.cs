8af3af7d5b2908b077aa8af58576db93ec18181a
Thomas Purtscher <61801240+GH-Thomas@users.noreply.github.com>
2025-05-03 14:09:51 -0400

    .NET Maui template import

---
+++ UltimateTierMaker/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace UltimateTierMaker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ UltimateTierMaker/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UltimateTierMaker
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
