5a96b8c7bc51a2e69ac4dc36f91c58c13522bafc
Thomas Purtscher <61801240+GH-Thomas@users.noreply.github.com>
2025-05-13 22:48:51 -0400

    possibly working item and tier

--- UltimateTierMaker/Views/ComponentViews/ItemView.xaml.cs
+++ UltimateTierMaker/Views/ComponentViews/ItemView.xaml.cs
@@ -1,9 +1,23 @@
 namespace UltimateTierMaker.Views.ComponentViews;
 
+using ItemModel = UltimateTierMaker.Models.Item;
+
 public partial class ItemView : ContentView
 {
-	public ItemView()
+    public static readonly BindableProperty ItemProperty = BindableProperty.Create(nameof(Item), typeof(ItemModel), typeof(ItemView), null);
+
+    public ItemModel Item
+    {
+        get => (ItemModel)GetValue(ItemView.ItemProperty);
+        set => SetValue(ItemView.ItemProperty, value);
+    }
+
+    public ItemView()
 	{
 		InitializeComponent();
+
+        itemImage.Source = Item.ImagePath;
+
+        itemLabel.Text = Item.Name;
 	}
 }
--- UltimateTierMaker/Views/ComponentViews/TierView.xaml.cs
+++ UltimateTierMaker/Views/ComponentViews/TierView.xaml.cs
@@ -1,9 +1,24 @@
 namespace UltimateTierMaker.Views.ComponentViews;
+using UltimateTierMaker.Models;
+
+using TierModel = UltimateTierMaker.Models.Tier;
 
 public partial class TierView : ContentView
 {
-	public TierView()
+    public static readonly BindableProperty TierProperty = BindableProperty.Create(nameof(Tier), typeof(TierModel), typeof(TierView), null);
+
+    public TierModel Tier
+    {
+        get => (TierModel)GetValue(TierView.TierProperty);
+        set => SetValue(TierView.TierProperty, value);
+    }
+
+    public TierView()
 	{
 		InitializeComponent();
+
+        listItems.ItemsSource = Tier.Items;
+
+        tierLabel.Text = Tier.Name;
 	}
 }
